80207b6b57964fa670c3383b960f2cc89746b7b1
Zack Argyle <zack.argyle@gmail.com>
2017-02-14 11:23:01 -0800

    Publish bugfix to 0.1.4 (#4)

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,6 +6,7 @@ const argv = require('yargs').argv;
 // Constants
 const packages = ['generate-service-worker', 'service-worker-plugin'];
 const semver = { major: 0, minor: 1, bugfix: 2 };
+var NEW_VERSION;
 
 // Update each package
 shell.echo(process.cwd());
@@ -18,10 +19,16 @@ packages.forEach(package => {
   const split = json.version.split('.').map(Number);
   const index = semver[argv.type || 'bugfix'];
   split[index] = (split[index] || 0) + 1;
-  json.version = split.join('.');
+  NEW_VERSION = split.join('.');
+  json.version = NEW_VERSION;
   fs.writeFileSync('./package.json', JSON.stringify(json, null, 2));
 
   // Publish to npm
   shell.exec('npm publish');
   shell.cd('..');
 });
+
+shell.exec(`git checkout -b publish-${NEW_VERSION}`);
+shell.exec('git add --all');
+shell.exec(`git commit -m "Publish ${argv.type} to ${NEW_VERSION}"`);
+shell.exec(`git push origin publish-${NEW_VERSION}`);
