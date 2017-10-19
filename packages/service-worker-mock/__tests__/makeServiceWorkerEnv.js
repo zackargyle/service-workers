cd651c2c46053a0bd194108099a00fa73f26cb36
Zack Argyle <zackargyle@pinterest.com>
2017-10-19 10:48:14 -0700

    Group tests

--- packages/service-worker-mock/__tests__/makeServiceWorkerEnv.js
+++ packages/service-worker-mock/__tests__/makeServiceWorkerEnv.js
@@ -16,35 +16,39 @@ describe('installation', () => {
     expect(self.location.href).toEqual('https://oh.yeah/scope');
   });
 
-  it('should allow resetting listeners', () => {
-    expect(Object.keys(self.listeners).length).toEqual(0);
-    self.addEventListener('fetch', () => {});
-    expect(Object.keys(self.listeners).length).toEqual(1);
-    self.listeners.reset();
-    expect(Object.keys(self.listeners).length).toEqual(0);
-  });
+  describe('environment resets', () => {
+    Object.assign(global, makeServiceWorkerEnv());
 
-  it('should allow resetting caches', async () => {
-    expect(await self.caches.has('TEST')).toBe(false);
-    self.caches.open('TEST');
-    expect(await self.caches.has('TEST')).toBe(true);
-    self.caches.reset();
-    expect(await self.caches.has('TEST')).toBe(false);
-  });
+    it('should allow resetting listeners', () => {
+      expect(Object.keys(self.listeners).length).toEqual(0);
+      self.addEventListener('fetch', () => {});
+      expect(Object.keys(self.listeners).length).toEqual(1);
+      self.listeners.reset();
+      expect(Object.keys(self.listeners).length).toEqual(0);
+    });
 
-  it('should allow resetting an individual cache', async () => {
-    const testResponse = new Response();
-    const cache = await self.caches.open('TEST');
-    await cache.put(new Request('/'), testResponse);
-    expect(await cache.match(new Request('/'))).toBe(testResponse);
-    cache.reset();
-    expect(await cache.match(new Request('/'))).toBe(null);
-  });
+    it('should allow resetting caches', async () => {
+      expect(await self.caches.has('TEST')).toBe(false);
+      await self.caches.open('TEST');
+      expect(await self.caches.has('TEST')).toBe(true);
+      self.caches.reset();
+      expect(await self.caches.has('TEST')).toBe(false);
+    });
 
-  it('should allow resetting clients', async () => {
-    const client = await self.clients.openWindow('/');
-    expect(await clients.get(client.id)).toBe(client);
-    clients.reset();
-    expect(await clients.get(client.id)).toBe(null);
+    it('should allow resetting an individual cache', async () => {
+      const testResponse = new Response();
+      const cache = await self.caches.open('TEST');
+      await cache.put(new Request('/'), testResponse);
+      expect(await cache.match(new Request('/'))).toBe(testResponse);
+      cache.reset();
+      expect(await cache.match(new Request('/'))).toBe(null);
+    });
+
+    it('should allow resetting clients', async () => {
+      const client = await self.clients.openWindow('/');
+      expect(await clients.get(client.id)).toBe(client);
+      clients.reset();
+      expect(await clients.get(client.id)).toBe(null);
+    });
   });
 });
