83935a81ae3a5c3554ec3b0a0adc5db69bdab9ed
Domingos Martins <domingos.martins@blip.pt>
2018-05-09 17:50:36 +0100

    add request chache, change Request to extend Body, fix Body, implement client.postMessage

--- packages/service-worker-mock/models/Client.js
+++ packages/service-worker-mock/models/Client.js
@@ -2,14 +2,19 @@ const generateRandomId = require('../utils/generateRandomId');
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Client
 class Client {
-  constructor(url, frameType) {
+  constructor(url, options) {
     this.id = generateRandomId();
     this.url = url;
-    this.frameType = frameType;
+    this.frameType = options ? options.frameType : undefined;
+    if (options && options.postMessageCallback) {
+      this.postMessageCallback = options.postMessageCallback;
+    } else {
+      this.postMessageCallback = message => { console.log(message); };
+    }
   }
 
-  postMessage() {
-    throw new Error('METHOD NOT IMPLEMENTED');
+  postMessage(message) {
+    this.postMessageCallback(message);
   }
 
   snapshot() {
