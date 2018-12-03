107f1ca05c53ff9fed0d2ad3ceef9d48e7331fb1
Zack Argyle <zackargyle@pinterest.com>
2018-12-03 09:31:42 -0800

    No support for object bodies

--- packages/service-worker-mock/models/Response.js
+++ packages/service-worker-mock/models/Response.js
@@ -1,15 +1,8 @@
 // stubs https://developer.mozilla.org/en-US/docs/Web/API/Response
 const Body = require('./Body');
 
-const isSupportedBodyType = (body) =>
-  (body instanceof Blob) ||
-  (typeof body === 'string');
-
 class Response extends Body {
   constructor(body, init) {
-    if (!isSupportedBodyType(body)) {
-      body = JSON.stringify(body);
-    }
     super(body);
     this.status = (init && typeof init.status === 'number') ? init.status : 200;
     this.ok = this.status >= 200 && this.status < 300;
