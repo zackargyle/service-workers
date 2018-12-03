0ceb055dc9f65fc7b4176ddf8d6c4ad77a583b96
Zack Argyle <zackargyle@pinterest.com>
2018-12-03 09:32:32 -0800

    Throw error for unsupported type

--- packages/service-worker-mock/models/Response.js
+++ packages/service-worker-mock/models/Response.js
@@ -1,8 +1,15 @@
 // stubs https://developer.mozilla.org/en-US/docs/Web/API/Response
 const Body = require('./Body');
 
+const isSupportedBodyType = (body) =>
+  (body instanceof Blob) ||
+  (typeof body === 'string');
+
 class Response extends Body {
   constructor(body, init) {
+    if (!isSupportedBodyType(body)) {
+      throw new TypeError('Response requires a Blob or USVString');
+    }
     super(body);
     this.status = (init && typeof init.status === 'number') ? init.status : 200;
     this.ok = this.status >= 200 && this.status < 300;
