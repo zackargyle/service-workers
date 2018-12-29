a91dd660883f4a1c3f51c8c70294c2374cc657c5
CJies Tan <cijies@gmail.com>
2018-12-29 17:24:38 +0800

    add WindowClient model

---
+++ packages/service-worker-mock/models/WindowClient.js
@@ -0,0 +1,23 @@
+const Client = require('./Client');
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowClient
+class WindowClient extends Client {
+  constructor(args) {
+    super(...args);
+
+    this.type = 'window';
+    this.focused = false;
+  }
+
+  focus() {
+    this.focused = true;
+    return Promise.resolve(this);
+  }
+
+  navigate(url) {
+    this.url = url;
+    return Promise.resolve(this);
+  }
+}
+
+module.exports = WindowClient;
