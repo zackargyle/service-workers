a7c4b3985620636c40e13b94bab54ccef1420f84
kommander <hasta84@gmail.com>
2019-01-16 00:24:45 +0100

    Use function with 200 response as default fetch mock

--- packages/service-worker-mock/fetch.js
+++ packages/service-worker-mock/fetch.js
@@ -1,1 +1,8 @@
-module.exports = () => ({ fetch: Promise.resolve() });
+module.exports = async (request) => {
+  const response = new Response('Response from service-worker-mock/fetch.js', {
+    status: 200,
+    statusText: 'ok.'
+  });
+  response.url = request.url;
+  return response;
+};
