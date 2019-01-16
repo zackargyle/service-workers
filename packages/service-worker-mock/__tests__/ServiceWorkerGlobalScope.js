1561db8aa19041d75b9072f3da4dcde2eae423e7
kommander <hasta84@gmail.com>
2019-01-16 01:32:26 +0100

    Add cacheName option for caches.match

--- packages/service-worker-mock/__tests__/ServiceWorkerGlobalScope.js
+++ packages/service-worker-mock/__tests__/ServiceWorkerGlobalScope.js
@@ -44,6 +44,22 @@ describe('installation', () => {
       expect(await cache.match(new Request('/'))).toBe(null);
     });
 
+    it('should allow cacheName option for caches.match', async () => {
+      const testResponse1 = new Response('body1');
+      const testResponse2 = new Response('body2');
+      const cache1 = await self.caches.open('TEST1');
+      const cache2 = await self.caches.open('TEST2');
+
+      await cache1.put(new Request('/'), testResponse1);
+      await cache2.put(new Request('/'), testResponse2);
+
+      const cacheResponse = await caches.match(new Request('/'), {
+        cacheName: 'TEST2'
+      });
+
+      expect(cacheResponse).toBe(testResponse2);
+    });
+
     it('should allow resetting clients', async () => {
       const client = await self.clients.openWindow('/');
       expect(await clients.get(client.id)).toBe(client);
