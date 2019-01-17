8ffd101fe5d1f3a2fb21066b6317b4e9f5da30ab
kommander <hasta84@gmail.com>
2019-01-17 18:41:34 +0100

    Replace deprecated Apache license notices with attribution notices

--- packages/service-worker-mock/models/ExtendableMessageEvent.js
+++ packages/service-worker-mock/models/ExtendableMessageEvent.js
@@ -1,10 +1,4 @@
-/*
-  Copyright 2018 Google LLC
-
-  Use of this source code is governed by an MIT-style
-  license that can be found in the LICENSE file or at
-  https://opensource.org/licenses/MIT.
-*/
+// Derived from https://github.com/GoogleChrome/workbox
 
 const ExtendableEvent = require('./ExtendableEvent');
 
