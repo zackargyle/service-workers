ceca49ecef147fe94ec9755eb1bc99a63ab2e8ed
Zack Argyle <zack.argyle@gmail.com>
2020-02-12 11:18:38 -0800

    Publish minor to 2.0.5 (#133)
    * Publish minor to 2.0.5

--- packages/service-worker-mock/models/FetchEvent.js
+++ packages/service-worker-mock/models/FetchEvent.js
@@ -6,20 +6,20 @@ const Request = require('./Request');
 // FetchEvent
 // https://www.w3.org/TR/service-workers-1/#fetch-event-section
 class FetchEvent extends ExtendableEvent {
-   constructor(type, init) {
-      super();
-      this.type = type;
-      this.isReload = init.isReload || false;
-      this.clientId = init.clientId || null;
-      if (init.request instanceof Request) {
-        this.request = init.request;
-      } else if (typeof init.request === 'string') {
-        this.request = new Request(init.request);
-      }
-   }
-   respondWith(response) {
-      this.promise = response;
-   }
+  constructor(type, init) {
+    super();
+    this.type = type;
+    this.isReload = init.isReload || false;
+    this.clientId = init.clientId || null;
+    if (init.request instanceof Request) {
+      this.request = init.request;
+    } else if (typeof init.request === 'string') {
+      this.request = new Request(init.request);
+    }
+  }
+  respondWith(response) {
+    this.promise = response;
+  }
 }
 
 module.exports = FetchEvent;
