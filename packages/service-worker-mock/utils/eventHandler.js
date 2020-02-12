8c58766669bd5eab3b0228c07b16de76b24b1487
Zachary Argyle <zackargyle@fb.com>
2020-02-12 11:02:05 -0800

    Fix lint errors

--- packages/service-worker-mock/utils/eventHandler.js
+++ packages/service-worker-mock/utils/eventHandler.js
@@ -5,46 +5,46 @@ const PushEvent = require('../models/PushEvent');
 const MessageEvent = require('../models/MessageEvent');
 
 function createEvent(event, args) {
-   switch (event) {
-      case 'fetch':
-         return new FetchEvent('fetch', getFetchArguments(args));
-      case 'notificationclick':
-         return new NotificationEvent(args);
-      case 'push':
-         return new PushEvent(args);
-      case 'message':
-         return new MessageEvent('message', args);
-      default:
-         return new ExtendableEvent();
-   }
+  switch (event) {
+    case 'fetch':
+      return new FetchEvent('fetch', getFetchArguments(args));
+    case 'notificationclick':
+      return new NotificationEvent(args);
+    case 'push':
+      return new PushEvent(args);
+    case 'message':
+      return new MessageEvent('message', args);
+    default:
+      return new ExtendableEvent();
+  }
 }
 
 function getFetchArguments(args) {
-   let request = args,
-      clientId = null;
+  let request = args;
+  let clientId = null;
 
-   if (typeof args === "object" && args.request) {
-      clientId = args.clientId || null;
-      request = args.request;
-   }
+  if (typeof args === 'object' && args.request) {
+    clientId = args.clientId || null;
+    request = args.request;
+  }
 
-   return {
-      request,
-      clientId
-   };
+  return {
+    request,
+    clientId
+  };
 }
 
 function handleEvent(name, args, callback) {
-   const event = createEvent(name, args);
-   callback(event);
-   return Promise.resolve(event.promise);
+  const event = createEvent(name, args);
+  callback(event);
+  return Promise.resolve(event.promise);
 }
 
 function eventHandler(name, args, listeners) {
-   if (listeners.length === 1) {
-      return handleEvent(name, args, listeners[0]);
-   }
-   return Promise.all(
+  if (listeners.length === 1) {
+    return handleEvent(name, args, listeners[0]);
+  }
+  return Promise.all(
       listeners.map(callback => handleEvent(name, args, callback))
    );
 }
